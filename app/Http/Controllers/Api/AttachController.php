41e7f363cc0df01baaa66fadf701d172dd280c83
carlosvechi <jrvechi@icloud.com>
2025-10-23 17:05:07 -0300

    FINALMENTE ANEXOS FUNCIONANDO

--- app/Http/Controllers/Api/AttachController.php
+++ app/Http/Controllers/Api/AttachController.php
@@ -8,75 +8,69 @@ use App\Models\Attach;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\FileBag;
 
-
 class AttachController extends Controller
 {
     public function index()
     {
-       return response()->json(Attach::All());
+        return response()->json(Attach::all());
     }
 
     public function store(Request $request)
     {
-      try{
         $validated = $request->validate([
-         'task_id' => 'required|exists:tasks,id',
-         'user_id' => 'required|exists:users,id',
-       ]);
+            'user_id' => 'required|exists:users,id',
+            'task_id' => 'required|exists:tasks,id',
+        ]);
 
-       $attachesFiles = $this->handleAttach($request->file('file_url'), $validated['user_id'], $validated['task_id']);
+        $attachesFiles = $this->handleAttach($request->file('file_url'), $validated['user_id'], $validated['task_id']);
         
         $attaches = [];
 
         foreach ($attachesFiles as $attachData) {
             $attaches[] = Attach::create($attachData);
         }
-      }catch(\Exception $e) {
-        return response()->json($e->getMessage(), 400);
-      }
-       
 
         return response()->json($attaches, 201);
     }
 
-    private function handleAttach($files, int $user_id, int $task_id) 
+    private function handleAttach($files, int $user_id, int $task_id)
     {
-      $attaches = [];
+        $attaches = [];
 
-      foreach ($files as $file) {
-        $path = $file->store('attachments/'. $task_id, 'local');
+        foreach ($files as $file) {
+            $path = $file->store('attachments/'. $task_id, 'local');
 
-        $attaches[] = [
-          'user_id' => $user_id,
-          'task_id' => $task_id,
-          'file_url' => Storage::disk('local')->path($path)
-        ];
-      }
+            $attaches[] = [
+                'user_id' => $user_id,
+                'task_id' => $task_id,
+                'file_url' => Storage::disk('local')->path($path),
+            ];
+        }
 
-      return $attaches;
+        return $attaches;
     }
 
     public function show(string $id)
     {
-       $attach = Attach::find($id);
+        $attaches = Attach::find($id);
 
-       if(!$attach){
-         return response()->json(['message' => 'Anexo não encontrado!'], 404);
-       }
+        if (!$attaches) {
+            return response()->json(['message' => 'Anexo não encontrado'], 404);
+        }
 
-       return response()->json($attach);
+        return response()->json($attaches);
+
     }
 
     public function destroy(string $id)
     {
-       $attach = Attach::find($id);
+        $attaches = Attach::find($id);
 
-       if(!$attach){
-         return response()->json(['message' => 'Anexo não encontrado!'], 404);
-       }
+        if (!$attaches) {
+            return response()->json(['message' => 'Anexo não encontrado'], 404);
+        }
 
-       $attach->delete();
-
-       return response()->json(['message' => 'Anexo excluído com sucesso!']);
+        $attaches->delete();
+        return response()->json(['message' => 'Anexo deletado com sucesso']);
     }
 }
